80e845a6a183f53a3a7f6d6d17b5de71f4a2757d
Unknown <suti211@gmail.com>
2017-05-10 16:15:42 +0200

    Extended interface to allow separate user tracking

--- src/model/IDataFucker.java
+++ src/model/IDataFucker.java
@@ -3,11 +3,11 @@ package model;
 import java.util.List;
 
 public interface IDataFucker {
-	List<Todo> getTodos();
-	List<Todo> getActiveTodos();
-	List<Todo> getInactiveTodos();
-	Todo addTodo(String name);
-	Todo getTodo(Integer id);
-	void deleteTodo(Integer id);
+	List<Todo> getTodos(List<Todo> list);
+	List<Todo> getActiveTodos(List<Todo> list);
+	List<Todo> getInactiveTodos(List<Todo> list);
+	Todo addTodo(String name, List<Todo> list);
+	Todo getTodo(Integer id, List<Todo> list);
+	void deleteTodo(Integer id, List<Todo> list);
 	void changeTodoState(Todo todo);
 }
